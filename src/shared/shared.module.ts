6b8941ec7472df536da727d84b1fb954449c0013
Kudi User <kudiuser@Kudis-MacBook-Pro.local>
2024-01-08 22:21:27 +0100

    Update

--- src/shared/shared.module.ts
+++ src/shared/shared.module.ts
@@ -1,37 +1,31 @@
 import { Module } from '@nestjs/common';
 import { TypeOrmModule } from '@nestjs/typeorm';
 
+console.log(process.env.DATABASE_NAME, "gsfdf")
 @Module({
   imports: [
     TypeOrmModule.forRoot({
         type: 'postgres',
-    host: 'localhost',
-    port: 5432,
-    username: 'postgres',
-    password: 'password@1A',
-    database: 'expense-tracker',
-
-      name: 'default',
-    //   type: process.env.TYPEORM_DB_TYPE as any,
-    //   host: process.env.TYPEORM_HOST,
-    //   port: Number(process.env.TYPEORM_PORT),
-    //   username: process.env.TYPEORM_USERNAME,
-    //   password: process.env.TYPEORM_PASSWORD,
-    //   database: process.env.TYPEORM_DATABASE_NAME,
-      logging: true,
-      // ssl:
-      //   process.env.ENVIRONMENT === 'development'
-      //     ? false
-      //     : {
-      //         rejectUnauthorized: false,
-      //       },
+        host: process.env.DATABASE_HOST,
+        port: Number(process.env.DATABASE_PORT),
+        username: process.env.DATABASE_USERNAME,
+        password: process.env.DATABASE_PASSWORD,
+        database: process.env.DATABASE_NAME,
+        name: 'default',
+        logging: true,
+        ssl:
+          process.env.ENVIRONMENT === 'development'
+            ? false
+            : {
+                rejectUnauthorized: false,
+              },
       entities: [__dirname + '/../**/*.entity.{js,ts}'],
       migrations: [__dirname + '/db/migrations/*'],
-      synchronize: true,
+      synchronize: process.env.ENVIRONMENT === 'development',
       dropSchema: false,
-      // extra: {
-      //   connectionLimit: 1,
-      // },
+      extra: {
+        connectionLimit: 1,
+      },
     }),
   ],
   controllers: [],
