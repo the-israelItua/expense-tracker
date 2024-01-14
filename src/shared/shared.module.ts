f3f92241284238fbc21392eab3c16d032e13c242
Kudi User <kudiuser@Kudis-MacBook-Pro.local>
2024-01-14 08:19:32 +0100

    Update

--- src/shared/shared.module.ts
+++ src/shared/shared.module.ts
@@ -21,7 +21,7 @@ console.log(process.env.DATABASE_NAME, "gsfdf")
               },
       entities: [__dirname + '/../**/*.entity.{js,ts}'],
       migrations: [__dirname + '/db/migrations/*'],
-      synchronize: true,
+      synchronize:  process.env.ENVIRONMENT === 'development',
       dropSchema: false,
       extra: {
         connectionLimit: 1,
